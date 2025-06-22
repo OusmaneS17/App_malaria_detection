006eaca1e547de659a841f877ba5a08030af6913
OusmaneS17 <oussoumanesow0@gmail.com>
2025-06-22 13:14:26 +0000

    first commit

--- malaria_detection/settings.py
+++ malaria_detection/settings.py
@@ -23,11 +23,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m-mino-y=1eqwqn$&5g5q_(d%n(bs+ow+!#op&d*wtljp9@y8@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'app-classification-cat.onrender.com',
+    'localhost',
+    '127.0.0.1',
+    ]
 
-
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 # Application definition
 
 INSTALLED_APPS = [
